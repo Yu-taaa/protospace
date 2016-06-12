0af29c3d3c1c8ffdd172d9358fba64f563bbc787
konndoyuta <konndoyuta@konndoyuta-no-MacBook-Pro.local>
2016-06-12 22:11:18 +0900

    edit a jsfile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,24 @@
 class UsersController < ApplicationController
 
   def index
+    @user = User.find(params[:id])
   end
 
   def show
   end
 
+  def update_params
+    current_user.update(update_params)
+    redirect_to action: show
+  end
+
   def edit
   end
+
+  private
+  def update_params
+    params.require(:user).permit(:email, :nickname, :member_name, :profile, :works)
+  end
+
+
 end
