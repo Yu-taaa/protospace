37935028fedc2906e28cf963d38a532e1d143857
konndoyuta <konndoyuta@konndoyuta-no-MacBook-Pro.local>
2016-07-17 15:25:18 +0900

    add delete function

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,5 +1,5 @@
 class Prototype < ActiveRecord::Base
-  has_many :images
+  has_many :images, dependent: :destroy
   belongs_to :user
   accepts_nested_attributes_for :images, allow_destroy: true, reject_if: proc { |attributes| attributes['image_url'].blank? }
   #validation
