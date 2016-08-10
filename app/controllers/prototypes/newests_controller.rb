f38e857c46e73d5c0bd29b4adaba091e46676b66
konndoyuta <konndoyuta@konndoyuta-no-MacBook-Pro.local>
2016-08-10 14:17:29 +0900

    edit newsts_controller and delete extra files

--- app/controllers/prototypes/newests_controller.rb
+++ app/controllers/prototypes/newests_controller.rb
@@ -1,6 +1,6 @@
 class Prototypes::NewestsController < ApplicationController
   def index
-    @prototypes = Prototype.includes(:user).page(params[:page]).per(5).order("created_at DESC")
+    @prototypes = Prototype.includes([:user, :images]).page(params[:page]).per(5).order("created_at DESC")
     @status = 'newest'
     render 'prototypes/index'
   end
